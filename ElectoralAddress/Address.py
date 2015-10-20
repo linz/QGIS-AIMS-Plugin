a4e29293682c48ec5f656e344b167158525341e0
Splanzer <splanzer@linz.govt.nz>
2015-10-20 22:14:38 +1300

    Initial commit

---
+++ ElectoralAddress/Address.py
@@ -0,0 +1,10 @@
+################################################################################
+#
+# Copyright 2015 Crown copyright (c)
+# Land Information New Zealand and the New Zealand Government.
+# All rights reserved
+#
+# This program is released under the terms of the new BSD license. See the 
+# LICENSE file for more information.
+#
+################################################################################
