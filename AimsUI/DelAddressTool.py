06dd5f925fca731ed9fed30d4dfb20690c61ba59
Joseph Ramsay <jramsay@linz.govt.nz>
2015-12-14 12:42:39 +1300

    t-ci build fix attempt #12

--- AimsUI/DelAddressTool.py
+++ AimsUI/DelAddressTool.py
@@ -1,11 +1,18 @@
 import sys
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+#from PyQt4.QtCore import *
+#from PyQt4.QtGui import *
 
-from qgis.core import *
-from qgis.gui import *
+#from qgis.core import *
+#from qgis.gui import *
+
+
+from qgis.gui import QgsMapTool
+from qgis.gui import QgsMapToolIdentify
 from qgis.gui import QgsMapToolIdentifyFeature as QMTIF
+
+from PyQt4.QtGui import QDialog
+from PyQt4.QtGui import QMessageBox
 
 #reload(QMTIF)
 
