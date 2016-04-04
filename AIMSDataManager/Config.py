a3b8c08c952ce521a030a28b1a4bb3c79f89a730
Joseph Ramsay <jramsay@linz.govt.nz>
2016-04-05 10:06:21 +1200

    config reader bugfixes

--- AIMSDataManager/Config.py
+++ AIMSDataManager/Config.py
@@ -27,7 +27,6 @@ class ConfigReader(object):
         self.cp.read(AIMS_CONFIG)
         self._readConfig()
         self._fillConfig()
-        #self._setConst()
         
     def _readConfig(self):
         '''Read the CP to a dict'''
@@ -35,16 +34,22 @@ class ConfigReader(object):
         for sect in self.cp.sections():
             self.d[sect] = {}
             for opt in self.cp.options(sect):
-                val = self.cp.get(sect,opt)
+                val = self._retype(self.cp.get(sect,opt).replace('"','').strip("'"))
                 self.d[sect][opt] = val or None
+                            
+    def _retype(self,val):    
+        if val.isdigit(): val = int(val)
+        elif val.replace('.','',1).isdigit(): val = float(val)
+        elif val.lower() in ('true','false'): val = bool(val)
+        return val
                 
     def _fillConfig(self):
         '''Attempt to fill missing values in config file with matching env vars'''
         #NOTE env vars must use aims_sec_opt=val format and are bypassed with null value
         for sect in self.d:
             for k,val in self.d[sect].items():
-                #NB Apply CC changes. Add test for text None (configparser *feature)
-                if val is None or val.strip() == '' or val == 'None' or all(i in whitespace for i in val):
+                #check to see if config items are blank, if they are search in env vars
+                if val is None or (isinstance(val,str) and ( val.strip() == '' or val == 'None' or all(i in whitespace for i in val) )):
                     envvar = 'aims_{}_{}'.format(sect,k)
                     eval = os.environ.get(envvar)
                     self.d[sect][k] = eval or DEF_CONFIG.get(sect,{}).get(k)
