2408c48130d038a6822df5b22680961029526f95
splanzer <splanzer@linz.govt>
2018-06-25 13:45:33 +1200

    ref doc. create app mview for par labelling

--- doc/createParcelView.sql
+++ doc/createParcelView.sql
@@ -1,28 +1,9 @@
--- Materialized View: lds.all_parcel_multipoly
-
--- DROP MATERIALIZED VIEW lds.all_parcel_multipoly;
-
-CREATE MATERIALIZED VIEW lds.all_parcel_multipoly AS 
- SELECT row_number() OVER () AS gid,
-    all_parcels.id,
-    all_parcels.appellation,
-    all_parcels.affected_surveys,
-    all_parcels.parcel_intent,
-    all_parcels.topology_type,
-    all_parcels.status,
-    all_parcels.statutory_actions,
-    all_parcels.land_district,
-    all_parcels.titles,
-    all_parcels.survey_area,
-    all_parcels.calc_area,
-    ST_SetSRID(st_multi(all_parcels.shape),4167) AS shape
-   FROM lds.all_parcels
-    WHERE (ST_GeometryType(shape) = 'ST_MultiPolygon'
-  OR  ST_GeometryType(shape) = 'ST_Polygon')
-  AND status = 'Current'
-WITH DATA;
-
-ALTER TABLE lds.all_parcel_multipoly
-  OWNER TO aims_dba;
-GRANT ALL ON TABLE lds.all_parcel_multipoly TO aims_dba;
-GRANT SELECT ON TABLE lds.all_parcel_multipoly TO aims_user;
+    DROP VIEW IF EXISTS bde.parcel_appellation_view;
+    CREATE OR REPLACE VIEW  bde.parcel_appellation_view AS 
+    SELECT bde.bde_get_combined_appellation(crs_parcel.id, 'N') AS appellation,
+           crs_parcel.id AS par_id
+    FROM   bde.crs_parcel
+    WHERE  bde.crs_parcel.status = 'CURR' 
+    AND    bde.crs_parcel.toc_code = 'PRIM';
+    ALTER TABLE bde.parcel_appellation_view OWNER TO bde_dba;
+    GRANT SELECT ON TABLE bde.parcel_appellation_view TO aims_user;
