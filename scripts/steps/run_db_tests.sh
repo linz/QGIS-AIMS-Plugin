f2da048c1ca1075b9ff16bada335fec21cf23b15
Sean McCann <SPM@harrisongrierson.com>
2024-04-19 11:35:41 +1200

    update sh scripts to print the directory info to make sure everything is pointing correctl

--- scripts/steps/run_db_tests.sh
+++ scripts/steps/run_db_tests.sh
@@ -7,9 +7,6 @@ echo >&2 "######################################"
 echo >&2 ""
 
 echo >&2 "Loading database_test.sql"
-# docker exec --privileged simple_db /bin/sh -c "cd / && ls -l"
-# docker exec --privileged simple_db /bin/sh -c "cd /simple_db_test && ls -l -R"
-# docker exec -u postgres simple_db psql -q -c "CREATE DATABASE simple_db;"
-# docker exec -u postgres simple_db psql -q -c "\c simple_db"
-# docker exec -u postgres simple_db psql -q -c "CREATE SCHEMA simple_schema;"
+docker exec --privileged db /bin/sh -c "cd / && ls -l"
+docker exec --privileged db /bin/sh -c "cd /QGIS-AIMS-Plugin && ls -l -R"
 docker exec -u postgres db psql -q -f /test/db/database_test.sql
--- scripts/steps/run_qgis_tests.sh
+++ scripts/steps/run_qgis_tests.sh
@@ -13,5 +13,9 @@ echo >&2 ""
 # --workdir /tests_directory \
 # qgis \
 # bash QGIS-AIMS-Plugin/test/run_tests.sh
+
+docker exec --privileged qgis /bin/sh -c "cd / && ls -l"
+docker exec --privileged qgis /bin/sh -c "cd /QGIS-AIMS-Plugin && ls -l -R"
+
 chmod +x /tests_directory/$PLUGIN_NAME/test/run_tests.sh
 docker exec --privileged -w /tests_directory/$PLUGIN_NAME qgis /bin/sh /tests_directory/$PLUGIN_NAME/test/run_tests.sh
