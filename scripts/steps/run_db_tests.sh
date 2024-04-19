12b4b64dd69593567710dade63de3c43b831a1df
Sean McCann <SPM@harrisongrierson.com>
2024-04-19 12:26:07 +1200

    still trying to fix path..

--- scripts/steps/run_db_tests.sh
+++ scripts/steps/run_db_tests.sh
@@ -9,4 +9,4 @@ echo >&2 ""
 echo >&2 "Loading database_test.sql"
 # docker exec --privileged db /bin/sh -c "cd / && ls -l"
 # docker exec --privileged db /bin/sh -c "cd /db_test && ls -l -R"
-docker exec -u postgres db psql -q -f /$PLUGIN_NAME/test/db/database_test.sql
+docker exec -u postgres db psql -q -f /db_test/$PLUGIN_NAME/test/db/database_test.sql
