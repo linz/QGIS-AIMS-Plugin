43ad51ba5fa25a06ed2a69ae259c26432af98988
Sean McCann <SPM@harrisongrierson.com>
2024-04-19 14:26:34 +1200

    Fixed reference to linux path handling in Config.py which is preventing the CI pipeline from reading this file.

--- QGIS-AIMS-Plugin/AIMSDataManager/Config.py
+++ QGIS-AIMS-Plugin/AIMSDataManager/Config.py
@@ -38,9 +38,11 @@ AIMS_CONFIG = os.path.join(QgsApplication.qgisSettingsDirPath(), "aims", "aimsCo
 # For Unit Testing, outside of QGIS, set path to your .ini file here as QgsApplication.qgisSettingsDirPath() resolves to '' if not called from QGIS
 if sys.platform == 'linux':
     # For testing via github actions, builds path to the repository aims_test_config.ini
-    AIMS_CONFIG = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'Test', 'aims_test_config.ini')
+    # AIMS_CONFIG = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'Test', 'aims_test_config.ini')
+    print(f'LINUX CI DETECTED - USING PATH: {AIMS_CONFIG}')
 
 if AIMS_CONFIG == 'aims\\aimsConfig.ini':
+    if os.path.exists()
     # Local testing 
     AIMS_CONFIG = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'Test', 'aims_test_config.ini')
 
--- scripts/steps/setup_qgis_plugin.sh
+++ scripts/steps/setup_qgis_plugin.sh
@@ -13,7 +13,4 @@ PROFILE_DIR=/root/.local/share/QGIS/QGIS3/profiles/default
 
 docker exec --privileged qgis mkdir -p $PROFILE_DIR/aims
 docker exec --privileged qgis cp /tests_directory/QGIS-AIMS-Plugin/test/aims_test_config.ini $PROFILE_DIR/aims/aimsConfig.ini
-
-docker exec --privileged qgis /bin/sh -c "cd /$PROFILE_DIR && ls -l -R"
-
 docker exec --privileged qgis bash -f /usr/bin/qgis_setup.sh "QGIS-AIMS-Plugin"
