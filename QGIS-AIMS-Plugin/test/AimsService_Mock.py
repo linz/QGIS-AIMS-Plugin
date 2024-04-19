81425bea72cc5a4ff374fe7da822c680da5d3963
Sean McCann <SPM@harrisongrierson.com>
2024-04-19 17:33:49 +1200

    removed print statement. Still unsure why the mock responses aren't continuing after DataManager_Test 5.20

--- QGIS-AIMS-Plugin/test/AimsService_Mock.py
+++ QGIS-AIMS-Plugin/test/AimsService_Mock.py
@@ -402,7 +402,6 @@ def mock_api_request(url:str, method, payload=None, headers=None, *args, **kwarg
     if content is None:
         raise LookupError(f'No URL path handling configured for: {url}')
 
-    print(f'API MOCK RESPONSE -- URL: {url} -- Method: {method} -- Payload: {payload} -- Response: {response} -- Content: {content}')
     return response, json.dumps(content)
 
 ###------
