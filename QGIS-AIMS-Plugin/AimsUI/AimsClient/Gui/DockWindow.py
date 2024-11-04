52a64872f841f6969c38fa860c7632fd137a78ad
Sean-McCann-HG <SPM@harrisongrierson.com>
2024-11-05 10:22:57 +1300

    Added fucntion for redocking the dockwidget in place, giving it focus and from what it appears, fixes scaling issues.

--- QGIS-AIMS-Plugin/AimsUI/AimsClient/Gui/DockWindow.py
+++ QGIS-AIMS-Plugin/AimsUI/AimsClient/Gui/DockWindow.py
@@ -13,20 +13,33 @@ from qgis.PyQt.QtGui import *
 from qgis.PyQt.QtCore import *
 from qgis.PyQt.QtWidgets import *
 from qgis.core import *
+from qgis.gui import QgisInterface
+from qgis.utils import iface
+iface:QgisInterface = iface
 
 class DockWindow( QDockWidget ):
     unloadPlugin = pyqtSignal()
     
     def __init__( self, parent, widget, name, title='' ):
         QDockWidget.__init__( self, parent )
+        self._parent = parent
         self._name = name
         if not title:
             title = name
         self.setWindowTitle( title )
         self.setWidget(widget)
-        parent.addDockWidget( Qt.LeftDockWidgetArea, self )
+        self.setObjectName('AIMSDockWidget')
+        self._parent.addDockWidget( Qt.LeftDockWidgetArea, self )
+        # iface.addDockWidget(Qt.LeftDockWidgetArea, self)
+        layerdock = self._parent.findChild(QDockWidget, "Layers")
+        self._parent.tabifyDockWidget(layerdock, self)
+        
         self.restoreLocation(True)
         self.topLevelChanged.connect( self.saveLocation )
+
+        # Set active tab
+        self.show()
+        self.raise_()
 
     def onTopLevelChanged( self, toplevel ):
         if self.isFloating():
@@ -40,6 +53,13 @@ class DockWindow( QDockWidget ):
     def moveEvent( self, event ):
         if self.isFloating():
             self.saveLocation()
+
+    def reDock(self):
+        self.setFloating(True)
+        self.setFloating(False)
+        self.show()
+        self.raise_()
+        # iface.addDockWidget(Qt.LeftDockWidgetArea, self.dockwidget)
 
     def saveLocation( self ):
         from Plugin import Plugin
